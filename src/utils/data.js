bb08a5081ba99b3b0365aa4db1684bfad69a72f2
agne-zavackyte <a.zavackyte@gmail.com>
2021-11-10 20:35:50 +0200

    data.js file created, ProcessListData variable added

---
+++ src/utils/data.js
@@ -0,0 +1,25 @@
+export const ProcessListData = [
+ {
+  id: 1,
+  title: "Build test task",
+  tasks: ["Create repository", "Implement designs", "Implement functionality"],
+ },
+ {
+  id: 2,
+  title: "Submit your test task",
+  tasks: [
+   "Open email client",
+   "Sent link with information to careers@cornercasetech.com",
+  ],
+ },
+ {
+  id: 3,
+  title: "Participate in tech interview",
+  tasks: ["Talk with HR", "Talk with Tech team"],
+ },
+ {
+  id: 4,
+  title: "Receive answer",
+  tasks: ["Receive answers", "Start your IT career"],
+ },
+];
