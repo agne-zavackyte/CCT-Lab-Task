7f37184e4d0393182f8f6ed15748ea066ad79b68
agne-zavackyte <a.zavackyte@gmail.com>
2021-11-10 20:58:56 +0200

    prop.processList added, dinamic funtionality added

--- src/components/ListItemBlock.tsx
+++ src/components/ListItemBlock.tsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import * as S from "./ListItemBlock.style";
 
-const ListItemBlock = () => {
+const ListItemBlock = (prop: {
+ processList: { id: number; title: string; tasks: string[] }[];
+}) => {
  const [display, setDisplay] = useState(false);
  return (
   <>
-   <S.TaskBlock isActive={display} onClick={() => setDisplay(!display)}>
-    <S.TaskId>1</S.TaskId>
-    <div>Title</div>
-   </S.TaskBlock>
-   <S.List isActive={display}>
-    <S.ListItem>One</S.ListItem>
-    <S.ListItem>Two</S.ListItem>
-   </S.List>
+   {prop.processList.map((listItem) => (
+    <div key={listItem.id}>
+     <S.TaskBlock isActive={display} onClick={() => setDisplay(!display)}>
+      <S.TaskId>{listItem.id}</S.TaskId>
+      <div>{listItem.title}</div>
+     </S.TaskBlock>
+     <S.List isActive={display}>
+      {listItem.tasks.map((li) => (
+       <S.ListItem key={li}>{li}</S.ListItem>
+      ))}
+     </S.List>
+    </div>
+   ))}
   </>
  );
 };
