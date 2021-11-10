3f6f2b815c16db2c542852f95f2497397650c4db
agne-zavackyte <a.zavackyte@gmail.com>
2021-11-10 21:03:10 +0200

    Selected item ID state created

--- src/components/ListItemBlock.tsx
+++ src/components/ListItemBlock.tsx
@@ -5,15 +5,28 @@ const ListItemBlock = (prop: {
  processList: { id: number; title: string; tasks: string[] }[];
 }) => {
  const [display, setDisplay] = useState(false);
+ /*
+  * Setting state for selected task. It controls, that only the choosen
+  * list is displayed
+  */
+ const [selection, setSelection] = useState(1);
+
  return (
   <>
    {prop.processList.map((listItem) => (
     <div key={listItem.id}>
-     <S.TaskBlock isActive={display} onClick={() => setDisplay(!display)}>
+     <S.TaskBlock
+      isActive={selection === listItem.id && display}
+      onClick={() => {
+       setSelection(listItem.id);
+       setDisplay(selection === listItem.id ? !display : true);
+      }}
+     >
       <S.TaskId>{listItem.id}</S.TaskId>
       <div>{listItem.title}</div>
      </S.TaskBlock>
-     <S.List isActive={display}>
+
+     <S.List isActive={listItem.id === selection && display}>
       {listItem.tasks.map((li) => (
        <S.ListItem key={li}>{li}</S.ListItem>
       ))}
