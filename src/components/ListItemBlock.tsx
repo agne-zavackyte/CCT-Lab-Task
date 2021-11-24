3900fef654fcd2f12bce2ee26950541f1f91b690
agne-zavackyte <a.zavackyte@gmail.com>
2021-11-24 14:02:04 +0200

    state update. Removed display state. Added mailto functionality

--- src/components/ListItemBlock.tsx
+++ src/components/ListItemBlock.tsx
@@ -2,33 +2,30 @@ import { useState } from "react";
 import * as S from "./ListItemBlock.style";
 
 const ListItemBlock = (prop: {
- processList: { id: number; title: string; tasks: string[] }[];
+ processList: { id: any | number; title: string; tasks: string[] }[];
 }) => {
- const [display, setDisplay] = useState(false);
- /*
-  * Setting state for selected task. It controls, that only the choosen
-  * list is displayed
-  */
- const [selection, setSelection] = useState(1);
+ const [selection, setSelection] = useState(null);
 
  return (
   <>
    {prop.processList.map((listItem) => (
     <div key={listItem.id}>
      <S.TaskBlock
-      isActive={selection === listItem.id && display}
+      isActive={selection === listItem.id}
       onClick={() => {
        setSelection(listItem.id);
-       setDisplay(selection === listItem.id ? !display : true);
       }}
      >
       <S.TaskId>{listItem.id}</S.TaskId>
       <div>{listItem.title}</div>
      </S.TaskBlock>
 
-     <S.List isActive={listItem.id === selection && display}>
-      {listItem.tasks.map((li) => (
-       <S.ListItem key={li}>{li}</S.ListItem>
+     <S.List isActive={listItem.id === selection}>
+      {listItem.tasks.map((item) => (
+       <S.ListItem
+        key={item}
+        dangerouslySetInnerHTML={{ __html: item }}
+       ></S.ListItem>
       ))}
      </S.List>
     </div>
